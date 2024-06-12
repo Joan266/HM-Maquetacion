cffdf3565be15b39a10ab2a6d196f0cba99218fa
Joan <joanalemany26@gmail.com>
2024-06-12 18:26:32 +0200

    added menu slider in food-menu home section

--- scripts/swiper-init.js
+++ scripts/swiper-init.js
@@ -1,9 +1,18 @@
-//  Initialize Swiper
-let swiper = new Swiper(".swiper--food", {
+
+let foodSwiper = new Swiper(".swiper--food", {
   slidesPerView: 1,
   initialSlide: 1, 
   loop:true,
   pagination: {
-    el: ".swiper-pagination--food",
+    el: ".swiper-pagination",
+  },
+});
+
+let menuSwiper = new Swiper(".swiper--menu", {
+  slidesPerView: 1,
+  loop:true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
   },
 });
