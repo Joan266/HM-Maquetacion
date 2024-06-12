003ad74c1bf8da782c6f469a571079804bbcbeac
Joan <joanalemany26@gmail.com>
2024-06-12 12:53:13 +0200

    added food slider to food section

---
+++ scripts/swiper-init.js
@@ -0,0 +1,9 @@
+//  Initialize Swiper
+let swiper = new Swiper(".swiper--food", {
+  slidesPerView: 1,
+  initialSlide: 1, 
+  loop:true,
+  pagination: {
+    el: ".swiper-pagination--food",
+  },
+});
