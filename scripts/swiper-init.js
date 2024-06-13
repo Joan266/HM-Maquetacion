8dc623868b3010a5cfcd191773d7a7fe080a1ad6
Joan <joanalemany26@gmail.com>
2024-06-13 12:34:53 +0200

    rooms sections for mobil done

--- scripts/swiper-init.js
+++ scripts/swiper-init.js
@@ -28,3 +28,16 @@ let facilitiesSwiper = new Swiper(".swiper--facilities", {
     el: ".swiper-pagination",
   },
 });
+let roomsSwiper = new Swiper(".swiper--rooms", {
+  slidesPerView: 1,
+  loop:true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  breakpoints: {
+    1000: {
+      slidesPerView: 3
+    }
+  }
+});
